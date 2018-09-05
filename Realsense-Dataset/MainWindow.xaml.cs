b734acbe6e91ef475c791b58e22cc5b86fc41102
Patrick Abadi <patrickabadi@gmail.com>
2018-09-05 15:24:43 -0700

    realsense input running

--- Realsense-Dataset/MainWindow.xaml.cs
+++ Realsense-Dataset/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using RSDS;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Realsense_Dataset
 {
@@ -20,9 +24,78 @@ namespace Realsense_Dataset
     /// </summary>
     public partial class MainWindow : Window
     {
+        private RsDsController _rsds;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void AddOuput(string txt)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Input, new ThreadStart(() =>
+            {
+                TxtOutput.Text = TxtOutput.Text.Insert(0, txt + "\r\n");
+            }));
+
+            Thread.Sleep(0);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _rsds = new RsDsController();
+
+            AddOuput("Initialized");
+        }
+
+        protected virtual void StatusCallback(RsDsController.Status status, string description)
+        {
+            Debug.WriteLine($"StatusCallback: {status} - {description}");
+
+            switch(status)
+            {
+                case RsDsController.Status.Initializing:
+                    AddOuput("Starting up...");
+                    BtnStartStop.IsEnabled = false;
+                    break;
+                case RsDsController.Status.Finalizing:
+                    AddOuput("Stopping...");
+                    BtnStartStop.IsEnabled = false;
+                    break;
+                case RsDsController.Status.RS_Started:
+                    AddOuput("Started");
+                    BtnStartStop.IsEnabled = true;
+                    BtnStartStop.Content = "Stop";
+                    break;
+                case RsDsController.Status.RS_Stopped:
+                    AddOuput("Stopped");
+                    BtnStartStop.IsEnabled = true;
+                    BtnStartStop.Content = "Start";
+                    break;
+                case RsDsController.Status.ErrorCameraUnplugged:
+                    AddOuput("ERROR: Camera unplugged");
+                    BtnStartStop.IsEnabled = true;
+                    BtnStartStop.Content = "Start";
+                    break;
+            }
+
+
+        }
+
+        private void BtnStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            switch (_rsds.State)
+            {
+                case RsDsController.StateType.Initializing:
+                case RsDsController.StateType.Stopping:
+                    return;
+                case RsDsController.StateType.Started:
+                    _rsds.Stop();
+                    break;
+                case RsDsController.StateType.Stopped:
+                    _rsds.Start();
+                    break;
+            }
         }
     }
 }
